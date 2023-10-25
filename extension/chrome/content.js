53adfa8a7073ecfed66b897116468444d5ebc7c4
simon <simon@clearfly.co.uk>
2023-10-25 23:36:18 +0100

    check button for auth url

--- extension/chrome/content.js
+++ extension/chrome/content.js
@@ -4,9 +4,13 @@
 let web3Buttons = document.getElementsByClassName('web3loginbutton');
 if(web3Buttons.length) {
   let button = null;
+  let url = window.location.href;
 
   web3Buttons[0].addEventListener('click', async (evt) => {
     button = evt.target;
+    if(button.dataset.url){
+      url = button.dataset.url;
+    }
 
     // using pollyfill browser ref - send message to SW
     // pass any data atrributes on button eg VC request type
@@ -19,14 +23,23 @@ if(web3Buttons.length) {
         alert('Select and present credential in Wallet first.');
       }else{
         // Post Auth request to self
-        fetch(window.location.href, {
+        fetch(url, {
           method: "POST",
           headers: {
             "Content-type": "application/json; charset=UTF-8",
             "Authentication": "web3credential " + r.data
           }
         }).then(res => {
-          console.log(res);
+          if(res.status == 200){
+            // if authed then reload. - TODO check for redirect url/header
+            if(confirm('Authentication complete.')){
+              window.location.reload();
+            }else{
+              window.location.reload();
+            }
+          }else{
+            alert('Authentication failed - try again');
+          }
         })
 
       }
